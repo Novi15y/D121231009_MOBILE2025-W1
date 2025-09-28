d87b92493952a461734a43138f2a58a05f8b3900
Novi15y <yulianin571@gmail.com>
2025-09-29 00:52:29 +0800

    Menghapus duplikat

--- week5/lib/controllers/post_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import '../models/post.dart';
-import '../repositories/post_repository.dart';
-
-class PostController extends ChangeNotifier{
-  final PostRepository repository;
-  
-}
