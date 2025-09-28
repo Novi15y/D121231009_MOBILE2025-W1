fc7434d85f23a9298b60697b565e3e835ef22f74
Novi15y <yulianin571@gmail.com>
2025-09-29 00:46:58 +0800

    Hand-on week 5

---
+++ week5/lib/controllers/post_controller.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+import '../models/post.dart';
+import '../repositories/post_repository.dart';
+
+class PostController extends ChangeNotifier{
+  final PostRepository repository;
+  
+}
